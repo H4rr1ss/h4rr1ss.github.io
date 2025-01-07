315544d08e1ccfeb027a17278070989f7d638671
H4rr1ss <3601320810101@ingenieria.usac.edu.gt>
2025-01-06 23:47:30 -0600

    feat: projects mobile

--- src/constants/const.ts
+++ src/constants/const.ts
@@ -24,5 +24,22 @@ export const dataProjects = [
     title: "Kehuel Airlines",
     description: "A comprehensive solution for airport management and security is provided, offering a seamless experience to passengers.",
     technologies: ["HTML5", "TypeScript", "NextJS", "TailwindCSS"],
+    urlImages: [
+      "/imas/kehuel1.jpeg",
+      "/imas/kehuel2.jpeg",
+      "/imas/kehuel3.jpeg",
+      "/imas/kehuel1.jpeg",
+    ]
+  },
+  {
+    title: "Qnave",
+    description: "Provide a safe, efficient and reliable transportation service in a challenging environment marked by safety issues and market competition.",
+    technologies: ["Angular 18", "TypeScript", "HTML5", "TailwindCSS"],
+    urlImages: [
+      "/imas/qnave1.png",
+      "/imas/qnave2.png",
+      "/imas/qnave3.png",
+      "/imas/qnave1.png",
+    ]
   }
 ]
